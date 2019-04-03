3d09ad6b93ec14ad790811a5942f8a2980aa9636
leonardo.marques <leonardo.marques@olxbr.com>
2019-04-03 02:57:36 -0300

    Navigation

--- app/src/main/java/br/com/olx/android/MainActivity.kt
+++ app/src/main/java/br/com/olx/android/MainActivity.kt
@@ -1,6 +1,5 @@
 package br.com.olx.android
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
@@ -8,14 +7,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        try {
-            val intent = Intent().setClassName(this,
-                "br.com.olx.listing.ListingActivity")
-            startActivity(intent)
-            finish()
-        } catch (e: ClassNotFoundException) {
-            e.printStackTrace()
-        }
+        setContentView(R.layout.activity_main)
     }
 }
