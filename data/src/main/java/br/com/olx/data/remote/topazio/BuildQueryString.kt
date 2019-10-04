cc07c6defadae2d7004c768cc16f41b12c246d97
Leonardo Marques <leonardo.marques@olxbr.com>
2019-10-04 16:28:10 -0300

    format buildQueryString

--- data/src/main/java/br/com/olx/data/remote/topazio/BuildQueryString.kt
+++ data/src/main/java/br/com/olx/data/remote/topazio/BuildQueryString.kt
@@ -1,5 +1,41 @@
 package br.com.olx.data.remote.topazio
 
 fun buildQueryString(queryParams: String): String {
-    return "{\"query\":\"query getAdsFromQueryString(\$queryString: String!) {    ads(queryString: \$queryString) {      pagination      ads {        listId        rankId        lastBumpAgeSecs        isFavorited        subject        origListTime        priceValue        oldPrice        professionalAd        featured        location {          neighbourhood          municipality          uf        }        images {          baseUrl          path        }        properties {          name          value        }        thumbnail {          baseUrl          path        }        user {          name          accountId        }        phone {          phone          phoneHidden          phoneVerified        }        adReply      }    }  }  \",\"variables\":{\"queryString\":\"$queryParams\"},\"operationName\":\"getAdsFromQueryString\"}"
+    return "{\"query\":\"query getAdsFromQueryString(\$queryString: String!) {    " +
+            "ads(queryString: \$queryString) {      " +
+            "pagination      " +
+            "ads {        " +
+            "listId        " +
+            "rankId        " +
+            "lastBumpAgeSecs        " +
+            "isFavorited        " +
+            "subject        " +
+            "origListTime        " +
+            "priceValue        " +
+            "oldPrice        " +
+            "professionalAd        " +
+            "featured        " +
+            "location {          " +
+            "neighbourhood          " +
+            "municipality          " +
+            "uf        }        " +
+            "images {          " +
+            "baseUrl          " +
+            "path        }        " +
+            "properties {          " +
+            "name          " +
+            "value        }        " +
+            "thumbnail {          " +
+            "baseUrl          " +
+            "path        }        " +
+            "user {          " +
+            "name          " +
+            "accountId        }        " +
+            "phone {          " +
+            "phone          " +
+            "phoneHidden          " +
+            "phoneVerified        }        " +
+            "adReply      }    }  }  \"" +
+            ",\"variables\":{\"queryString\":\"$queryParams\"}" +
+            ",\"operationName\":\"getAdsFromQueryString\"}"
 }
